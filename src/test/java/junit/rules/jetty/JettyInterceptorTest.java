8421f72f2f813105b766c9ac44fe52150e411670
Alistair A. Israel <aisrael@gmail.com>
2009-10-22 11:42:35 +0800

    Added tests again (broken)

---
+++ src/test/java/junit/rules/jetty/JettyInterceptorTest.java
@@ -0,0 +1,68 @@
+/**
+ * junit-rules: JUnit Rules Library
+ *
+ * Copyright (c) 2009 by Alistair A. Israel.
+ * This software is made available under the terms of the MIT License.
+ *
+ * Created Aug 28, 2009
+ */
+package junit.rules.jetty;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mortbay.jetty.handler.AbstractHandler;
+
+/**
+ * JUnit test case for {@link JettyInterceptor}
+ *
+ * @author Alistair A. Israel
+ */
+public final class JettyInterceptorTest {
+
+    // CHECKSTYLE:OFF
+    @Rule
+    public final JettyInterceptor jetty = new JettyInterceptor();
+    // CHECKSTYLE:ON
+
+    /**
+     * @throws Exception
+     *         on exception
+     */
+    @Test
+    public void testHttpServerInterceptor() throws Exception {
+        jetty.setHandler(new AbstractHandler() {
+
+            @Override
+            public void handle(final String target, final HttpServletRequest request,
+                    final HttpServletResponse response, final int dispatch) throws IOException,
+                    ServletException {
+                final PrintWriter out = response.getWriter();
+                out.println("<?xml version=\"1.0\"?>");
+                out.println("<resource id=\"1234\" name=\"test\" />");
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.flushBuffer();
+            }
+        });
+
+        final URL url = new URL("http://localhost:" + jetty.getPort() + "/1234.xml");
+        final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        final BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        assertEquals("<?xml version=\"1.0\"?>", in.readLine());
+        assertEquals("<resource id=\"1234\" name=\"test\" />", in.readLine());
+        assertEquals(HTTP_OK, connection.getResponseCode());
+    }
+}
---
+++ src/test/java/junit/rules/jpa/HibernatePersistenceContextTest.java
@@ -0,0 +1,65 @@
+/**
+ * junit-rules: JUnit Rules Library
+ *
+ * Copyright (c) 2009 by Alistair A. Israel.
+ * This software is made available under the terms of the MIT License.
+ *
+ * Created Oct 15, 2009
+ */
+package junit.rules.jpa;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import junit.rules.db.Fixtures;
+import junit.rules.jpa.hibernate.HibernatePersistenceContext;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.example.ejb3.beans.WidgetBean;
+import com.example.model.Widget;
+
+/**
+ * @author Alistair A. Israel
+ * @since 0.3
+ */
+@Fixtures("fixtures.xml")
+public final class HibernatePersistenceContextTest {
+
+    // CHECKSTYLE:OFF
+    @Rule
+    public static HibernatePersistenceContext persistenceContext = new HibernatePersistenceContext();
+    // CHECKSTYLE:ON
+
+    private WidgetBean widgetBean = new WidgetBean();
+
+    /**
+     * @throws Exception
+     *         on exception
+     */
+    @Test
+    
+    public void testListAll() throws Exception {
+        persistenceContext.injectAndPostConstruct(widgetBean);
+
+        final List<Widget> widgets = widgetBean.listAll();
+        assertNotNull(widgets);
+        assertEquals(2, widgets.size());
+    }
+
+    /**
+     * @throws Exception
+     *         on exception
+     */
+    @Test
+    public void testFindById() throws Exception {
+        persistenceContext.injectAndPostConstruct(widgetBean);
+
+        final Widget widget = widgetBean.findById(2);
+        assertNotNull(widget);
+        assertEquals(2, widget.getId().intValue());
+    }
+}
