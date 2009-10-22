0fbf6ed6ef79453f544591ae0bb900eaa96c476e
Alistair A. Israel <aisrael@gmail.com>
2009-10-22 12:35:11 +0800

    Added AllTests

---
+++ src/test/java/junit/rules/AllTests.java
@@ -0,0 +1,30 @@
+/**
+ * junit-rules: JUnit Rules Library
+ *
+ * Copyright (c) 2009 by Alistair A. Israel.
+ * This software is made available under the terms of the MIT License.
+ *
+ * Created Oct 22, 2009
+ */
+package junit.rules;
+
+import junit.rules.httpserver.HttpServerInterceptorTest;
+import junit.rules.jetty.JettyInterceptorTest;
+import junit.rules.jpa.hibernate.HibernatePersistenceContextTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Alistair A. Israel
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+    HttpServerInterceptorTest.class,
+    JettyInterceptorTest.class,
+    HibernatePersistenceContextTest.class }
+)
+public class AllTests {
+
+}
